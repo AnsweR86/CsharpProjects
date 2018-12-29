673b38dab13dedf8364d27878d6a4115be4f852c
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2018-12-29 02:21:32 -0800

    Writing and Running in C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+
+
+    class Program
+    {
+    
+
+    public static int Accounting { get; private set; }
+
+    static void Main()
+    {
+        Console.WriteLine("Welocome to Acme Accounting System");
+
+        Console.WriteLine("Remember we'r 'accountin'on you!");
+        Console.ReadLine();
+
+
+    }
+   
+    }
+
